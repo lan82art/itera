f32efbf23b19ffe029f0c315d7f1b0a4771e305f
Artem <lanart@ex.ua>
2017-07-28 15:38:04 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="css/style.css" rel="stylesheet">
+    <title>Itera</title>
+</head>
+<body>
+<div class="row">
+    <div class="col _1"><h1>1</h1></div>
+    <div class="col _2"><h1>2</h1></div>
+    <div class="col _3"><h1>3</h1></div>
+    <div style="clear:both"></div>
+</div>
+<div class="row">
+    <div class="col _4"><h1>4</h1></div>
+    <div class="col _5"><h1>5</h1></div>
+    <div class="col _6"><h1>6</h1></div>
+    <div style="clear:both"></div>
+</div>
+<div class="row">
+    <div class="col _7"><h1>7</h1></div>
+    <div class="col _8"><h1>8</h1></div>
+    <div class="col _9"><h1>9</h1></div>
+    <div style="clear:both"></div>
+</div>
+</body>
+</html>
